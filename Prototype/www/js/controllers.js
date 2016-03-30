241c04a1c943aaa5eb132a3fc363b081580692f4
amydentremont <aldentre@umass.edu>
2016-03-30 15:33:51 -0400

    Prototype from Ionic Creator
    :)

--- Prototype/www/js/controllers.js
+++ Prototype/www/js/controllers.js
@@ -1,13 +1,17 @@
 angular.module('app.controllers', [])
-     
+  
 .controller('homeCtrl', function($scope) {
 
 })
    
+.controller('logMoodCtrl', function($scope, $ionicPopup, $timeout) {
+
+})
+
 .controller('moodLogCtrl', function($scope) {
 
 })
-   
+      
 .controller('trackProgressCtrl', function($scope) {
 
 })
