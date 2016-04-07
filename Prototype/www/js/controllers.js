b8819eeed70f8cb6934e0a31ac6db6064c609704
Benjamin Tseytlin <btseytlin@gmail.com>
2016-04-07 14:17:42 -0400

    Can add moods to mood log service, and view them in mood log. Range doesn't come through in data structures, also need to reload page from localhost:8100 sometimes or else switching templates doesn't work

--- Prototype/www/js/controllers.js
+++ Prototype/www/js/controllers.js
@@ -1,111 +1,33 @@
 angular.module('app.controllers', [])
-
+  
 .controller('homeCtrl', function($scope) {
-  $scope.defaultMoods = [{
-    mood: 'Angry'
-  },
-  {
-    mood: 'Sad'
-  },{
-    mood: 'Happy'
-  },{
-    mood: 'Stressed'
-  },{
-    mood: 'Hungry'
-  },{
-    mood: 'Anxious'
-  },{
-    mood: 'Jealous'
-  },{
-    mood: 'Depressed'
-  },{
-    mood: 'Tired'
-  }];
-
-  $scope.AddMood = function (data) {
-  $scope.MoodList.push({
-    mood: data.newMood,
-    trigger: data.newTrigger,
-    comment: data.newComment
-  });
-  data.newItem = '';
-};
 
 })
+   
+.controller('logMoodCtrl', ['$scope', 'MoodLogService','CreateMoodLogEntryService', function($scope, MoodLogService,CreateMoodLogEntryService) {
 
-.controller('logMoodCtrl', function($scope, $ionicPopup, $timeout) {
+	$scope.mood ='';
+	$scope.range = '';
+	$scope.trigger ='';
+	$scope.beliefs ='';
+	$scope.behavior ='';
+	$scope.comments ='';
 
-})
+	//when submit button clicked (in logMood.html there's a reference to this method)
+	$scope.submitLog = function(){ 
+		MoodLogService.addMoodData(CreateMoodLogEntryService.createMoodLogEntry(this.mood, this.range, this.trigger, this.beliefs, this.behavior, this.comments));
+		console.log("Submit(). here's the data in our MoodLog:");
+		console.log(MoodLogService.getMoodData());
+	};
+}])
+   
+.controller('moodLogCtrl', ['$scope', 'MoodLogService', function($scope, MoodLogService) {
+	//display the mood in moodLog.html
+	$scope.entries = MoodLogService.getMoodData();
 
-.controller('moodLogCtrl', function($scope) {
-  $scope.MoodList = [{
-        mood: 'Angry',
-        trigger: '',
-        comment: ''
-      }, {
-        mood: 'Sad',
-        trigger: '',
-        comment: ''
-      }, {
-        mood: 'Happy',
-        trigger: '',
-        comment: ''
-      }, {
-        mood: 'Stressed',
-        trigger: '',
-        comment: ''
-      }, {
-        mood: 'Hungry',
-        trigger: '',
-        comment: ''
-      }, {
-        mood: 'Anxious',
-        trigger: '',
-        comment: ''
-      },
-      {
-        mood: 'Jealous',
-        trigger: '',
-        comment: ''
-      },
-      {
-        mood: 'Depressed',
-        trigger: '',
-        comment: ''
-      },
-      {
-        mood: 'Tired',
-        trigger: '',
-        comment: ''
-      }];
-
-})
-
+}])
+      
 .controller('trackProgressCtrl', function($scope) {
 
 })
-
-//controller for a modal, thinking about using it for submit mood
-.controller('modalController', function($scope, $ionicModal) {
-  $ionicModal.fromTemplateUrl('my-modal.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-  $scope.openModal = function() {
-    $scope.modal.show();
-  };
-  $scope.closeModal = function() {
-    $scope.modal.hide();
-  };
-  //Cleanup the modal when we're done with it!
-  $scope.$on('$destroy', function() {
-    $scope.modal.remove();
-  });
-  // Execute action on hide modal
-  $scope.$on('modal.hidden', function() {
-    // Execute action
-  });
-
-});
+ 
--- Prototype/www/js/services.js
+++ Prototype/www/js/services.js
@@ -6,5 +6,50 @@ angular.module('app.services', [])
 
 .service('BlankService', [function(){
 
-}]);
+}])
+
+.service('MoodLogService', [function(){
+
+	//call this service to update the logs we have
+
+	//moodData contains MoodLogEntries, which are created by using the CreateMoodLogEntryService
+	this.moodData = [];
+
+	//get the full list of logs, in order of their creation
+	this.getMoodData = function(){
+		return this.moodData;
+	}
+
+	//add new log to the data
+	this.addMoodData = function(mood){
+		this.moodData.push(mood);
+	}
+
+}])
+
+.service('CreateMoodLogEntryService', [function(){
+
+	//a mood log entry
+	//todo: time of creation...etc.
+	class MoodLogEntry{
+		constructor(mood, range, trigger, beliefs, behavior, comments){
+			this.mood = mood;
+			this.range = range;
+			this.trigger = trigger;
+			this.beliefs = beliefs;
+			this.behavior = behavior;
+			this.comments = comments;
+		}
+	}
+
+	//returns a new instance of MoodLogEntry with given params
+	this.createMoodLogEntry = function(mood, range, trigger, beliefs, behavior, comments){
+		return new MoodLogEntry(mood, range, trigger, beliefs, behavior, comments);
+	}
+
+}])
+
+
+
+;
 
