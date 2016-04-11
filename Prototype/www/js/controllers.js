9bee94497dd220ebc03171ed56df5618ea319945
amydentremont <aldentre@umass.edu>
2016-04-11 14:29:29 -0400

    New branch amybranch
    Seperate from amy's branch

--- Prototype/www/js/controllers.js
+++ Prototype/www/js/controllers.js
@@ -1,10 +1,108 @@
 angular.module('app.controllers', [])
 
 .controller('homeCtrl', function($scope) {
+
+  $scope.showSettings = function() {
+  	$scope.data = {}
+  	var customPopup = $ionicPopup.show({
+  		title: 'Settings',
+  		template: 'setting stuff',
+  		scope: $scope,
+  		buttons: [
+  			{ text: 'Cancel' },
+  			{ 
+  			  text: '<b>Save</b>',
+  			  type: 'button-positive'
+  			  //onTap
+  			},
+  		]
+  	});
+  	customPopup.then(function(res) {
+
+  	})
+  };
+
+})
+
+.controller('logMoodCtrl', function($scope, $ionicPopup, $timeout) {
+
+  $scope.showSettings = function() {
+  	$scope.data = {}
+  	var customPopup = $ionicPopup.show({
+  		title: 'Settings',
+  		template: 'Settings Stuff <br> Here\'s an input box:<input type="text" ng-model="data.settings">',
+  		scope: $scope,
+  		buttons: [
+  			{ text: 'Cancel' },
+  			{ 
+  			  text: '<b>Save</b>',
+  			  type: 'button-positive'
+  			  //onTap
+  			},
+  		]
+  	});
+  	customPopup.then(function(res) {
+
+  	})
+  };
+
+  $scope.showCreateNewMood = function() {
+  	$scope.data = {}
+  	var customPopup = $ionicPopup.show({
+  		title: 'Create New Mood',
+  		template: 'Enter new mood name<input type="text" ng-model="data.newMoodName">',
+  		// positive or negative mood toggle thing
+  		scope: $scope,
+  		buttons: [
+  			{ text: 'Cancel' },
+  			{
+  			  text: '<b>Add</b>',
+  			  type: 'button-positive',
+  			  //onTap: function(e)
+  			},
+  		]
+  	});
+  	customPopup.then(function(res) {
+  		if(res) {
+  			console.log('Name has been entered');
+
+  		} else {
+  			console.log('Name has not bee entered');
+  		}
+  	})
+  };
+
+  $scope.showSubmit = function() {
+  	// should actually check if fields are valid first
+  	var alertPopup = $ionicPopup.alert({
+  		title: 'Submit',
+  		template: 'Mood logged successfully!'
+  	});
+  	alertPopup.ten(function(res) {
+  		console.log('Done');
+  	});
+  };
+
+  $scope.showCancel = function() {
+  	var confirmPopup = $ionicPopup.confirm({
+  		title: 'Cancel',
+  		template: 'Are you sure you wish to cancel?'
+  	});
+  	confirmPopup.then(function(res) {
+  		if(res) {
+  			console.log('You clicked the OK button');
+  			//tabsController.select(1);
+  		} else {
+  			console.log('You clicked the Cancel button');
+  		}
+  	});
+  };
+
+
+
   $scope.defaultMoods = [{
     mood: 'Angry'
-  },
-  {
+  },{
     mood: 'Sad'
   },{
     mood: 'Happy'
@@ -22,6 +120,7 @@ angular.module('app.controllers', [])
     mood: 'Tired'
   }];
 
+  //should this be submit instead?
   $scope.AddMood = function (data) {
   $scope.MoodList.push({
     mood: data.newMood,
@@ -30,51 +129,40 @@ angular.module('app.controllers', [])
   });
   data.newItem = '';
 };
-
-})
-
-.controller('logMoodCtrl', function($scope, $ionicPopup, $timeout) {
-
 })
 
 .controller('moodLogCtrl', function($scope) {
+
+  $scope.showSettings = function() {
+  	$scope.data = {}
+  	var customPopup = $ionicPopup.show({
+  		title: 'Settings',
+  		template: 'setting stuff <br> Heres an input box<input type="text" ng-model="data.settings">',
+  		scope: $scope,
+  		buttons: [
+  			{ text: 'Cancel' },
+  			{ 
+  			  text: '<b>Save</b>',
+  			  type: 'button-positive'
+  			  //onTap
+  			},
+  		]
+  	});
+  	customPopup.then(function(res) {
+
+  	})
+  };
+
   $scope.MoodList = [{
-        mood: 'Angry',
+        mood: 'Logged Mood 1',
+        trigger: 'Mood trigger 1',
+        comment: 'Mood comment 1'
+      }, {
+        mood: 'Logged Mood 2',
         trigger: '',
         comment: ''
       }, {
-        mood: 'Sad',
-        trigger: '',
-        comment: ''
-      }, {
-        mood: 'Happy',
-        trigger: '',
-        comment: ''
-      }, {
-        mood: 'Stressed',
-        trigger: '',
-        comment: ''
-      }, {
-        mood: 'Hungry',
-        trigger: '',
-        comment: ''
-      }, {
-        mood: 'Anxious',
-        trigger: '',
-        comment: ''
-      },
-      {
-        mood: 'Jealous',
-        trigger: '',
-        comment: ''
-      },
-      {
-        mood: 'Depressed',
-        trigger: '',
-        comment: ''
-      },
-      {
-        mood: 'Tired',
+        mood: 'Logged Mood 3',
         trigger: '',
         comment: ''
       }];
@@ -82,6 +170,26 @@ angular.module('app.controllers', [])
 })
 
 .controller('trackProgressCtrl', function($scope) {
+
+  $scope.showSettings = function() {
+  	$scope.data = {}
+  	var customPopup = $ionicPopup.show({
+  		title: 'Settings',
+  		template: 'setting stuff',
+  		scope: $scope,
+  		buttons: [
+  			{ text: 'Cancel' },
+  			{ 
+  			  text: '<b>Save</b>',
+  			  type: 'button-positive'
+  			  //onTap
+  			},
+  		]
+  	});
+  	customPopup.then(function(res) {
+
+  	})
+  };
 
 })
 
