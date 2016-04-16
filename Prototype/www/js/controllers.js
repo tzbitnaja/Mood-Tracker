cf415c466026aa53da2b66830328ff3bf5140cfa
Kalju Lee <kvlee@acad.umass.edu>
2016-04-16 19:08:44 -0400

    made a chart, stuck on services

--- Prototype/www/js/controllers.js
+++ Prototype/www/js/controllers.js
@@ -1,111 +1,126 @@
 angular.module('app.controllers', [])
 
-.controller('homeCtrl', function($scope) {
-  $scope.defaultMoods = [{
-    mood: 'Angry'
-  },
-  {
-    mood: 'Sad'
-  },{
-    mood: 'Happy'
-  },{
-    mood: 'Stressed'
-  },{
-    mood: 'Hungry'
-  },{
-    mood: 'Anxious'
-  },{
-    mood: 'Jealous'
-  },{
-    mood: 'Depressed'
-  },{
-    mood: 'Tired'
-  }];
+.controller('homeCtrl', function ($scope) {
+    $scope.defaultMoods = [{
+        mood: 'Angry'
+    },
+    {
+        mood: 'Sad'
+    }, {
+        mood: 'Happy'
+    }, {
+        mood: 'Stressed'
+    }, {
+        mood: 'Hungry'
+    }, {
+        mood: 'Anxious'
+    }, {
+        mood: 'Jealous'
+    }, {
+        mood: 'Depressed'
+    }, {
+        mood: 'Tired'
+    }];
 
-  $scope.AddMood = function (data) {
-  $scope.MoodList.push({
-    mood: data.newMood,
-    trigger: data.newTrigger,
-    comment: data.newComment
-  });
-  data.newItem = '';
-};
-
-})
-
-.controller('logMoodCtrl', function($scope, $ionicPopup, $timeout) {
+    $scope.AddMood = function (data) {
+        $scope.MoodList.push({
+            mood: data.newMood,
+            trigger: data.newTrigger,
+            comment: data.newComment
+        });
+        data.newItem = '';
+    };
 
 })
 
-.controller('moodLogCtrl', function($scope) {
-  $scope.MoodList = [{
+.controller('logMoodCtrl', function ($scope, $ionicPopup, $timeout) {
+
+})
+
+.controller('moodLogCtrl', function ($scope) {
+    $scope.MoodList = [{
         mood: 'Angry',
         trigger: '',
         comment: ''
-      }, {
+    }, {
         mood: 'Sad',
         trigger: '',
         comment: ''
-      }, {
+    }, {
         mood: 'Happy',
         trigger: '',
         comment: ''
-      }, {
+    }, {
         mood: 'Stressed',
         trigger: '',
         comment: ''
-      }, {
+    }, {
         mood: 'Hungry',
         trigger: '',
         comment: ''
-      }, {
+    }, {
         mood: 'Anxious',
         trigger: '',
         comment: ''
-      },
-      {
-        mood: 'Jealous',
-        trigger: '',
-        comment: ''
-      },
-      {
-        mood: 'Depressed',
-        trigger: '',
-        comment: ''
-      },
-      {
-        mood: 'Tired',
-        trigger: '',
-        comment: ''
-      }];
+    },
+        {
+            mood: 'Jealous',
+            trigger: '',
+            comment: ''
+        },
+        {
+            mood: 'Depressed',
+            trigger: '',
+            comment: ''
+        },
+        {
+            mood: 'Tired',
+            trigger: '',
+            comment: ''
+        }];
 
 })
-
-.controller('trackProgressCtrl', function($scope) {
-
-})
+    // displays line graphs of mood intensity and date
+.controller('trackProgressCtrl', ['$scope', function ($scope, Number) {
+    $scope.someText = Number;
+    //$scope.labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
+    //$scope.series = ["Series A", "Series B"];
+    //$scope.data = [
+    //    [65, 59, 80, 81, 56, 55, 40],
+    //    [28, 48, 40, 18, 86, 27, 90]
+    //];
+    //$scope.onClick = function (points, evt) {
+    //    console.log(points, evt);
+    //};
+    //$scope.moodSelection = function () {
+    //    $scope.data = [
+    //        [10, 12, 15],
+    //        [22, 10, 8]
+    //    ];
+    //};
+}])
 
 //controller for a modal, thinking about using it for submit mood
-.controller('modalController', function($scope, $ionicModal) {
-  $ionicModal.fromTemplateUrl('my-modal.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-  $scope.openModal = function() {
-    $scope.modal.show();
-  };
-  $scope.closeModal = function() {
-    $scope.modal.hide();
-  };
-  //Cleanup the modal when we're done with it!
-  $scope.$on('$destroy', function() {
-    $scope.modal.remove();
-  });
-  // Execute action on hide modal
-  $scope.$on('modal.hidden', function() {
-    // Execute action
-  });
+.controller('modalController', function ($scope, $ionicModal) {
+    $ionicModal.fromTemplateUrl('my-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+    $scope.openModal = function () {
+        $scope.modal.show();
+    };
+    $scope.closeModal = function () {
+        $scope.modal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function () {
+        $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function () {
+        // Execute action
+    });
 
 });
--- Prototype/www/js/services.js
+++ Prototype/www/js/services.js
@@ -1,10 +1,45 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
+.factory('BlankFactory', [function () {
 
 }])
 
-.service('BlankService', [function(){
+.service('BlankService', [function () {
 
+}])
+
+    // (eventually) grabs the mood data from localStorage
+    // for now, just returns some stock mood data
+.factory('Number', [function () {
+    var number = 8;
+    return number;
+    //var m1 = {
+    //     mood: 'angry',
+    //     degree: 10,
+    //     notes: "",
+    //     date: new Date(2016, 1, 2, 4)
+    // }
+
+    //var m2 = {
+    //    mood: 'angry',
+    //    degree: 8,
+    //    notes: "",
+    //    date: new Date(2016, 1, 4, 4)
+    //};
+
+    //var m3 = {
+    //    mood: 'angry',
+    //    degree: 5,
+    //    notes: "",
+    //    date: new Date(2016, 1, 10, 4)
+    //};
+
+    //var m4 = {
+    //    mood: 'angry',
+    //    degree: 10,
+    //    notes: "",
+    //    date: new Date(2016, 1, 13, 4)
+    //};
+
+    //var recs = [m1, m2, m3, m4];
 }]);
-
